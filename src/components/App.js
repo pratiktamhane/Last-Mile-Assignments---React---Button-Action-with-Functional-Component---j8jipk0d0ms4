855e1e3bebfd08e4674cb0e2baf5a42286e67ad4
pratiktamhane <96224999+pratiktamhane@users.noreply.github.com>
2022-03-20 20:20:17 +0530

    button

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [state, setState] = useState(false);
   return (
     <div id="main">
-      // Do not alter the main div
+      <button
+        id="click"
+        onClick={() => {
+          setState(true);
+        }}
+      >
+        {""}
+        click
+      </button>
+
+      {state === true ? (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ) : null}
     </div>
   );
 }
-
 
 export default App;
